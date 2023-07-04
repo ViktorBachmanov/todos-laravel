d1bd6b2315996ea2afe2caae53e3861626641be6
Vikt0r <home@home.ru>
2023-07-04 20:22:18 +0300

    Edit login.blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,36 @@
-<div>
-  Login
-</div>
+<x-layout>
+
+  <!-- Validation Errors -->
+  @error('password')
+      <div>{{ $message }}</div>
+  @enderror
+
+  <form method="POST" action="{{ route('login') }}">
+      @csrf
+
+      <!-- Email Address -->
+      <div>
+          <label for="email">Email</label>
+
+          <input id="email" type="email" name="email" value="{{ old('email') }}" required />
+      </div>
+
+      <!-- Password -->
+      <div>
+          <label for="password">Пароль</label>
+
+          <input id="password" 
+                          type="password"
+                          name="password"
+                          required autocomplete="current-password" />
+      </div>
+
+      
+      <div>
+          <button>
+              Войти
+          </button>
+      </div>
+  </form>
+   
+</x-layout>
