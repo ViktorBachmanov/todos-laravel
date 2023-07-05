1bd60b0706fcdfc0abaa72dc6db67127200a465b
Vikt0r <home@home.ru>
2023-07-05 08:41:00 +0300

    Install Bootstrap-Icons

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,12 +7,19 @@
         <title>Todo - Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> -->
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     </head>
     <body class="antialiased">
+      <div class="navbar navbar-expand-lg bg-body-tertiary">
+        <button class="btn btn-primary" aria-label="Add Todo">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+          </svg>
+        </button>
+      </div>
         {{ $slot }}
     </body>
 </html>
