6fdba57d7929fc7115c5b9b7cd18e63d26c412c8
Vikt0r <home@home.ru>
2023-07-06 09:51:07 +0300

    Establish 'new-todo' and 'edit-todo' custom events

--- resources/views/components/edit-todo-modal.blade.php
+++ resources/views/components/edit-todo-modal.blade.php
@@ -18,6 +18,9 @@
 
 
 <script>
+  const editTodoModal = document.getElementById('edit-todo-modal');
+  const $editTodoModal = $("#edit-todo-modal");
+
   const saveButton = document.getElementById('save-todo');
 
   saveButton.onclick = async function() {
@@ -29,8 +32,52 @@
 
     window.todo.add(data);
 
-    // const modal = new window.bootstrap.Modal(document.getElementById('edit-todo-modal'));
-    // modal.toggle();
-    $("#edit-todo-modal").modal("hide");
+    $editTodoModal.modal("hide");
+  }
+
+  const todosContainer = document.getElementById('todos');
+  todosContainer.addEventListener('click', async function(e) {
+    console.log('target: ', e.target);
+
+    if(!e.target.classList.contains('edit-button')) {
+      return;
+    }
+
+    const todoId = e.target.closest(".card").dataset.id;
+
+    const { data } = await axios.get(`/todos/${todoId}`);
+    console.log("data: ", data);
+
+    const editTodoEvent = new CustomEvent("edit-todo", {
+        detail: {
+            text: data.text,
+        },
+    });
+
+    editTodoModal.dispatchEvent(editTodoEvent);
+  })
+
+  editTodoModal.addEventListener('edit-todo', editTodo);
+
+  function editTodo(e) {
+    const todoTextEl = document.getElementById('todo-text');
+    todoTextEl.value = e.detail.text;
+
+    $editTodoModal.modal("show");
+  }
+
+
+  const newTodoButton = document.getElementById('new-todo-button');
+  newTodoButton.onclick = () => {
+    editTodoModal.dispatchEvent(new CustomEvent("new-todo"));
+  }
+
+  editTodoModal.addEventListener('new-todo', newTodo);
+
+  function newTodo(e) {
+    const todoTextEl = document.getElementById('todo-text');
+    todoTextEl.value = '';
+
+    $editTodoModal.modal("show");
   }
 </script>
