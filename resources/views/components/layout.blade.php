b2683a7a8b272ae54f5876b4130a9702d71decbd
Vikt0r <home@home.ru>
2023-07-08 09:30:36 +0300

    Replace asset('css/app.css') link with mix('css/app.css') one

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
         <title>Todo - Laravel</title>
 
         
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
         <script src="{{ mix('/js/app.js') }}"></script>
 
