797b751c6533dd4557501745bdd26f967d96f85f
Vikt0r <home@home.ru>
2023-07-08 21:15:10 +0300

    Create tags-filter.blade component

---
+++ resources/views/components/tags-filter.blade.php
@@ -0,0 +1,10 @@
+<div style="max-width: 10em; flex: 1 0 auto">
+  Tags filter
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Tag" id="tags-filter-input" aria-label="Tag filter">
+      <button class="btn btn-outline-secondary" type="button" id="tags-filter-add-button">
+        <i class="bi bi-plus"></i>
+      </button>
+    </div>
+  <div id="tags-filter-container"></div>
+</div>
